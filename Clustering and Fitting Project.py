4da3c0a0422e16a721cae461b30ce301c089e69b
seyifalope <126199071+seyifalope@users.noreply.github.com>
2023-05-11 17:55:48 +0100

    Add files via upload
    Second stage of coding

--- Clustering and Fitting Project.py
+++ Clustering and Fitting Project.py
@@ -48,3 +48,124 @@ urban = urban[urban['Country Name'].isin(['China', 'Ghana', 'Kenya', 'United Sta
 # Dropping the columns not needed
 urban = urban.drop(
     ['Indicator Name', 'Country Code', 'Indicator Code'], axis=1)
+
+# reseting the index
+urban.reset_index(drop=True, inplace=True)
+
+# Extracting  years from our urban out dataset
+
+urban_s = urban[['Country Name', '1990', '2000', '2010', '2019']]
+print(urban_s.describe())
+
+# Checking for missing values
+print(urban_s.isna().sum())
+
+# Transposing the data
+urban_t = urban_s.T
+
+urban_t.columns = urban_t.iloc[0]  # setting the columns
+urban_t = urban_t.iloc[1:]  # filtering for only years
+print(urban_t.describe())
+urban_t = urban_t.apply(pd.to_numeric)  # converting the datatype to numeric
+urban_t.plot()
+
+
+# Extracting 30 years of data at an interval of 10 years from out dataset
+urban_year = urban_s[['1990', '2000', '2010', '2019']]
+urban_year.describe()
+
+# Checking for missing values
+urban_year.isna().sum()
+
+# Checking for any missing values
+urban_year.isna().sum()
+
+# dropping the missing values
+urban_year.dropna(inplace=True)
+
+# Checking for correlation between our years choosen
+
+# Correlation
+corr = urban_year.corr()
+corr
+# heatmap
+ct.map_corr(urban_year)
+
+print(corr)
+
+# scatter plot
+pd.plotting.scatter_matrix(urban_year, figsize=(9.0, 9.0))
+plt.tight_layout()    # helps to avoid overlap of labels
+plt.show()
+
+urban_cluster = urban_year[['1990', '2019']].copy()
+
+print(urban_cluster)
+
+# Normalizing the data and storing minimum and maximum value
+urban_norm, urban_min, urban_max = ct.scaler(urban_cluster)
+print(urban_norm.describe())
+
+
+# Calculating the best clustering number
+for i in range(2, 9):
+    # creating  kmeans and fit
+    kmeans = cluster.KMeans(n_clusters=i)
+    kmeans.fit(urban_cluster)
+
+    # extract labels and calculate silhoutte score
+    labels = kmeans.labels_
+    print(i, skmet.silhouette_score(urban_cluster, labels))
+
+"""    
+2 and 3 has the highest silhoutte score respectively , 
+so i will be plotting for 2 and 3 clusters and choosing the best
+"""
+
+# Plotting for 3 clusters
+nclusters = 3 # number of cluster centres
+
+kmeans = cluster.KMeans(n_clusters=nclusters)
+kmeans.fit(urban_norm)     
+
+# extract labels and cluster centres
+labels = kmeans.labels_
+
+# extracting the estimated number of cluster
+cen = kmeans.cluster_centers_
+
+plt.figure(figsize=(6.0, 6.0))
+# scatter plot with colours selected using the cluster numbers
+plt.scatter(urban_norm["1990"], urban_norm["2019"], c=labels, cmap="tab10")
+# colour map Accent selected to increase contrast between colours
+
+# show cluster centres
+xcen = cen[:,0]
+ycen = cen[:,1]
+plt.scatter(xcen, ycen, c="k", marker="d", s=80)
+# c = colour, s = size
+
+plt.xlabel("Urban(1990)")
+plt.ylabel("Urban(2019)")
+plt.title("3clusters")
+plt.show()
+
+# Scaling back to the original data and creating a plot it on the original scale
+
+plt.style.use('default')
+plt.figure(dpi=300)
+
+# now using the original dataframe
+plt.scatter(urban_s["1990"], urban_s["2019"], c=labels, cmap="tab10")
+
+
+# rescale and show cluster centres
+scen = ct.backscale(cen, urban_min, urban_max)
+xc = scen[:,0]
+yc = scen[:,1]
+plt.scatter(xc, yc, c="k", marker="d", s=80)
+
+plt.xlabel("1990")
+plt.ylabel("2020")
+plt.title("Urban population (% of total population)")
+plt.show()
